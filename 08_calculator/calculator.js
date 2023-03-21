23822fcf9321b276781657fb23a946849d3545a9
Kenref <107287396+Kenref@users.noreply.github.com>
2023-03-21 17:19:35 +1100

    Completed Calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b
+};
+
+const subtract = function(a, b) {
+	return a - b
+};
+
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
+}
+
+
+const multiply = function(array) {
+  return array.reduce((a, b) => a * b, 1);
+}
+
+const power = function(a, b) {
+  return Math.pow(a, b)
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1
+  if (num => 1) {
+    for (let i = 1; i <= num; i++) {
+      total *= i
+    }
+  }
+  return total
 };
 
 // Do not edit below this line
