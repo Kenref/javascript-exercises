0b697d021692486e9a930b2bbdd32a456e4f51c0
Kenref <107287396+Kenref@users.noreply.github.com>
2023-03-22 18:38:36 +1100

    Completed Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = (word) => {
+    const reg = /\W/g
 
-};
+    word = word.toLowerCase()
+    word = word.replace(reg, "")
+
+    let word2 = word.split("").reverse().join("")
+
+    return word === word2
+
+}
 
 // Do not edit below this line
 module.exports = palindromes;
