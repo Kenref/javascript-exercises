68ec151272b27d22ff48dc439b4884c4568cf6bf
Kenref <107287396+Kenref@users.noreply.github.com>
2023-03-23 17:09:49 +1100

    Completed Fibonacci exercise (to review later)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,11 @@
-const fibonacci = function() {
+ function fibonacci(number) {
+  if (number > 0) {
+    if (number < 3) return 1;
+    return fibonacci(number-1) + fibonacci(number-2)
+  }
+  else {
+    return "OOPS"
+  }
 
 };
 
