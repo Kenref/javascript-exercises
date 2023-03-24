5ffb411020bbf9063d9a4c8bbe89186a412444e2
Kenref <107287396+Kenref@users.noreply.github.com>
2023-03-24 12:25:06 +1100

    Completed findTheOldest with Reduce method

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const result = people.reduce((array, person) => {
+        const death = person.yearOfDeath, birth = person.yearOfBirth
+        let age = death - birth
+        array.push(age)
+        return array
+    },[])
+    let oldest = Math.max.apply(null, result)
+    oldest = result.indexOf(oldest)
+    return people[oldest]
+}
 
-};
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
+    //   const people = [
+    //     {
+    //       name: "Carly",
+    //       yearOfBirth: 1942,
+    //       yearOfDeath: 1970,
+    //     },
+    //     {
+    //       name: "Ray",
+    //       yearOfBirth: 1962,
+    //       yearOfDeath: 2011,
+    //     },
+    //     {
+    //       name: "Jane",
+    //       yearOfBirth: 1912,
+    //       yearOfDeath: 1941,
+    //     },
+    //   ]
