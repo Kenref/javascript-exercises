e2750c4c28be42811835c20ad9911259a1c3ab72
Kenref <107287396+Kenref@users.noreply.github.com>
2023-03-24 12:45:39 +1100

    Completed findTheOldest Exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,13 @@
 const findTheOldest = function(people) {
     const result = people.reduce((array, person) => {
         const death = person.yearOfDeath, birth = person.yearOfBirth
-        let age = death - birth
+        let age = 0
+        if (death == undefined) {
+            age = new Date().getFullYear() - birth
+        }
+        else {
+            age = death - birth
+        }
         array.push(age)
         return array
     },[])
@@ -13,22 +19,3 @@ const findTheOldest = function(people) {
 
 // Do not edit below this line
 module.exports = findTheOldest;
-
-
-    //   const people = [
-    //     {
-    //       name: "Carly",
-    //       yearOfBirth: 1942,
-    //       yearOfDeath: 1970,
-    //     },
-    //     {
-    //       name: "Ray",
-    //       yearOfBirth: 1962,
-    //       yearOfDeath: 2011,
-    //     },
-    //     {
-    //       name: "Jane",
-    //       yearOfBirth: 1912,
-    //       yearOfDeath: 1941,
-    //     },
-    //   ]
